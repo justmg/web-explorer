0de8a9ec8402c35e4ec16a5d0732b4a0adca3329
justmg <aykyassini@gmail.com>
2023-08-15 20:47:19 -0500

    Update web_explorer.py

--- web_explorer.py
+++ web_explorer.py
@@ -1,93 +1,28 @@
 import streamlit as st
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.retrievers.web_research import WebResearchRetriever
+from langchain.llms.openai import OpenAI
+from langchain.agents import load_tools, initialize_agent
 
+# Streamlit app
+st.subheader('LangChain Search')
 
+# Get OpenAI API key, SERP API key and search query
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+serpapi_api_key = st.text_input("SERP API Key", type="password")
+search_query = st.text_input("Search Query")
 
-st.set_page_config(page_title="Interweb Explorer", page_icon="🌐")
-
-def settings():
-
-    # Vectorstore
-    import faiss
-    from langchain.vectorstores import FAISS 
-    from langchain.embeddings.openai import OpenAIEmbeddings
-    from langchain.docstore import InMemoryDocstore  
-    embeddings_model = OpenAIEmbeddings()  
-    embedding_size = 1536  
-    index = faiss.IndexFlatL2(embedding_size)  
-    vectorstore_public = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
-
-    # LLM
-    from langchain.chat_models import ChatOpenAI
-    llm = ChatOpenAI(model_name="gpt-3.5", temperature=0, streaming=True)
-
-    # Search
-    from langchain.utilities import GoogleSearchAPIWrapper
-    search = GoogleSearchAPIWrapper()   
-
-    # Initialize 
-    web_retriever = WebResearchRetriever.from_llm(
-        vectorstore=vectorstore_public,
-        llm=llm, 
-        search=search, 
-        num_search_results=3
-    )
-
-    return web_retriever, llm
-
-class StreamHandler(BaseCallbackHandler):
-    def __init__(self, container, initial_text=""):
-        self.container = container
-        self.text = initial_text
-
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text += token
-        self.container.info(self.text)
-
-
-class PrintRetrievalHandler(BaseCallbackHandler):
-    def __init__(self, container):
-        self.container = container.expander("Context Retrieval")
-
-    def on_retriever_start(self, query: str, **kwargs):
-        self.container.write(f"**Question:** {query}")
-
-    def on_retriever_end(self, documents, **kwargs):
-        # self.container.write(documents)
-        for idx, doc in enumerate(documents):
-            source = doc.metadata["source"]
-            self.container.write(f"**Results from {source}**")
-            self.container.text(doc.page_content)
-
-
-st.sidebar.image("img/ai.png")
-st.header("`Interweb Explorer`")
-st.info("`I am an AI that can answer questions by exploring, reading, and summarizing web pages."
-    "I can be configured to use different modes: public API or private (no data sharing).`")
-
-# Make retriever and llm
-if 'retriever' not in st.session_state:
-    st.session_state['retriever'], st.session_state['llm'] = settings()
-web_retriever = st.session_state.retriever
-llm = st.session_state.llm
-
-# User input 
-question = st.text_input("`Ask a question:`")
-
-if question:
-
-    # Generate answer (w/ citations)
-    import logging
-    logging.basicConfig()
-    logging.getLogger("langchain.retrievers.web_research").setLevel(logging.INFO)    
-    qa_chain = RetrievalQAWithSourcesChain.from_chain_type(llm, retriever=web_retriever)
-
-    # Write answer and sources
-    retrieval_streamer_cb = PrintRetrievalHandler(st.container())
-    answer = st.empty()
-    stream_handler = StreamHandler(answer, initial_text="`Answer:`\n\n")
-    result = qa_chain({"question": question},callbacks=[retrieval_streamer_cb, stream_handler])
-    answer.info('`Answer:`\n\n' + result['answer'])
-    st.info('`Sources:`\n\n' + result['sources'])
+# If the 'Search' button is clicked
+if st.button("Search"):
+    # Validate inputs
+    if not openai_api_key.strip() or not serpapi_api_key.strip() or not search_query.strip():
+        st.error(f"Please provide the missing fields.")
+    else:
+        try:
+            with st.spinner('Please wait...'):
+              # Initialize the OpenAI module, load the SerpApi tool, and run the search query using an agent
+              llm=OpenAI(temperature=0, openai_api_key=openai_api_key, verbose=True)
+              tools = load_tools(["serpapi"], llm, serpapi_api_key=serpapi_api_key)
+              agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+              result = agent.run(search_query)
+              st.success(result)
+        except Exception as e:
+            st.exception(f"An error occurred: {e}")
